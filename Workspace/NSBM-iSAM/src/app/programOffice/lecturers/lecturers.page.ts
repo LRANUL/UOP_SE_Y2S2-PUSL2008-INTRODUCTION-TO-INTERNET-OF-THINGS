9e249e027271eea4c404ae70f83b10722c33e321
Lucas Lahiru.H.H <hvlhasanka@students.nsbm.lk>
2020-03-04 16:58:37 +0530

    W9U8
    NSBM iSAM Management System -
    Improvements to the frontend of main screen.
    Implementation of the frontend and backend of lecturer registration.

--- Workspace/NSBM-iSAM/src/app/programOffice/lecturers/lecturers.page.ts
+++ Workspace/NSBM-iSAM/src/app/programOffice/lecturers/lecturers.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-lecturers',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LecturersPage implements OnInit {
 
-  constructor() { }
+  lecturerRegistrationForm: FormGroup;
+
+  constructor(
+    private lecturerReg: FirebaseService,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+
+    this.lecturerRegistrationForm = this.formBuilder.group({
+      firstName: new FormControl(''),
+      middleName: new FormControl(''),
+      lastName: new FormControl(''),
+      specialization: new FormControl(''),
+      faculty: new FormControl(''),
+      nsbmEmail: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      confirmPassword: new FormControl('', Validators.compose([
+        Validators.required
+      ]))
+    });
+
+  }
+
+  doLecturerRegistration(value) {
+
+    // Firebase auth
+    this.lecturerReg.lecturerRegistrationDetails(value)
+      .then(success => {
+        console.log(" (Firebase Auth) Lecturer Credentials Registration Successful, " + success);
+      }, 
+      error => {
+        console.log(" (Firebase Auth) Lecturer Credentials Registration Failed, " + error);
+      });
+  
   }
 
 }
--- Workspace/NSBM-iSAM/src/app/services/firebase.service.ts
+++ Workspace/NSBM-iSAM/src/app/services/firebase.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import * as firebase from 'firebase/app';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 
 @Injectable({
@@ -11,7 +12,10 @@ export class FirebaseService {
   
   navCtrl: any;
 
-  constructor(private firestore: AngularFirestore) {
+  constructor(
+    private firestore: AngularFirestore
+    ) {
+
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         // User is signed in.
@@ -22,6 +26,7 @@ export class FirebaseService {
         console.log('User is NOT signed in');
       }
     });
+
   }
   
   
@@ -61,6 +66,37 @@ export class FirebaseService {
   userDetails() {
     return firebase.auth().currentUser;
   }
+
+
+  // Implementation of Registering a new lecturer into the system (firebase authentication)
+  lecturerRegistrationDetails(value){
+    return new Promise<any>((resolve, reject) => {
+      // Adding new record into firebase auth
+      firebase.auth().createUserWithEmailAndPassword(value.nsbmEmail, value.confirmPassword)
+        .then(
+
+          success => {
+            console.log(" Lecturer UserID: " + success.user.uid);
+
+            // Adding lecturer details into firestore
+            this.firestore.collection("lecturers").add({
+              userID: success.user.uid, // Retrieving UID of newly added user
+              name: {
+                firstName: value.firstName,
+                middleName: value.middleName,
+                lastName: value.lastName,
+              },
+              specialization: value.specialization,
+              faculty: value.faculty
+            })
+          },
+          error => reject(error)
+
+        )
+    })
+     
+  }
+
 }
 
 
